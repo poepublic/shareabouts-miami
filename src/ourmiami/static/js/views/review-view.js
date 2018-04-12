fcb14c1f025a9a68f0945dc6ab4255248e8eb432
Mjumbe Poe <mjumbewu@gmail.com>
2018-04-12 18:15:41 -0400

    First pass at duplicating ratings js and jstemplates for staff reviews

---
+++ src/ourmiami/static/js/views/review-view.js
@@ -0,0 +1,147 @@
+/*globals _ Handlebars Backbone */
+
+var Shareabouts = Shareabouts || {};
+
+(function(S, $, console){
+  'use strict';
+
+  S.ReviewView = Backbone.View.extend({
+    events: {
+      'change .evaluation': 'onEvaluationChange',
+      'change input[name=review]': 'onReviewChange',
+      'submit .user-review': 'onFormSubmit'
+    },
+
+    initialize: function() {
+      this.collection.on('reset', this.onChange, this);
+      this.collection.on('add', this.onChange, this);
+      this.collection.on('remove', this.onChange, this);
+    },
+
+    render: function() {
+      var self = this,
+          userReview,
+
+          // The userStaffGroup is set in an app initializer in index.html
+          // NOTE: Treat all places as being in the current staff group
+          userStaffGroup = S.Config.userStaffGroup;
+
+      if (!userStaffGroup || !userStaffGroup.length) {
+        return this;
+      }
+
+      // I don't understand why we need to redelegate the events here, but they
+      // are definitely unbound after the first render.
+      this.delegateEvents();
+
+      // NOTE: All reviews use modify the same reviews
+      userReview = this.getReview();
+      this.$el.html(Handlebars.templates['place-detail-review']({
+        count: this.collection.size() || '',
+        user_token: this.options.userToken,
+        user_review: userReview.toJSON(),
+        place_id: this.collection.options.placeModel.id
+      }));
+
+      return this;
+    },
+
+    remove: function() {
+      // Nothing yet
+    },
+
+    unbindReview: function() {
+      if (this.userReview) { this.userReview.off('change', this.onChange, this); }
+    },
+    bindReview: function() {
+      if (this.userReview) { this.userReview.on('change', this.onChange, this); }
+    },
+
+    getReview: function() {
+      var reviews = this.collection,
+          ReviewModel = reviews.model,
+          model = this.collection.last();
+
+      if (!this.userReview || (model && this.userReview !== model)) {
+        this.unbindReview();
+        this.userReview = model || new ReviewModel();
+        this.bindReview();
+      }
+
+      if (this.userReview.isNew()) {
+        this.userReview.urlRoot = reviews.url();
+      }
+
+      return this.userReview;
+    },
+
+    onChange: function() {
+      this.render();
+    },
+
+    onFormSubmit: function(evt) {
+      evt.preventDefault();
+      this.saveReview();
+    },
+
+    onReviewNotesBlur: function(evt) {
+      var userReview = this.getReview(),
+          oldNotes = userReview.get('review_notes'),
+          newNotes = $(evt.currentTarget).val();
+
+      if (newNotes !== oldNotes) {
+        this.saveReview();
+      }
+    },
+
+    onEvaluationChange: function(evt) {
+      var review = evt.target.getAttribute('data-review-value');
+
+      S.Util.log('USER', 'place', 'review-btn-click', this.collection.options.placeModel.getLoggingDetails(), review);
+      this.$('input[name=review]').val(review).trigger('change');
+    },
+
+    onReviewChange: function(evt) {
+      this.saveReview();
+    },
+
+    saveReview: function() {
+      var self = this,
+          review = this.$('input[name=review]').val(),
+          evalButtons = this.el.getElementsByClassName('evaluation'),
+          notesWidget = $('[name=review_notes]'),
+          $form, attrs,
+          reviews = this.collection,
+          ReviewModel = reviews.model,
+          userReview = this.getReview();
+
+      // Disable the evaluation widgets while we save; they'll be enabled again on complete
+      _.map(evalButtons, function(button) { button.disabled = true; });
+      notesWidget.each(function(i, widget) { widget.disabled = false; });
+
+      $form = this.$('form');
+      attrs = S.Util.getAttrs($form);
+
+      userReview.save(attrs, {
+        wait: true,
+        beforeSend: function(xhr) {
+          // Set the silent header so that review doesn't generate activity
+          xhr.setRequestHeader('X-Shareabouts-Silent', 'true');
+        },
+        complete: function() {
+          _.map(evalButtons, function(button) { button.disabled = false; });
+          notesWidget.each(function(i, widget) { widget.disabled = false; });
+        },
+        success: function() {
+          reviews.add(userReview);
+          S.Util.log('USER', 'place', 'successfully-review', self.collection.options.placeModel.getLoggingDetails());
+        },
+        error: function() {
+          alert('Oh dear. It looks like that didn\'t save.');
+          S.Util.log('USER', 'place', 'fail-to-review', self.collection.options.placeModel.getLoggingDetails());
+        }
+      });
+    }
+  });
+
+})(Shareabouts, jQuery, Shareabouts.Util.console);
---
+++ src/ourmiami/static/js/views/reviews-summary-view.js
@@ -0,0 +1,271 @@
+/*globals _ Handlebars Backbone */
+
+var Shareabouts = Shareabouts || {};
+
+(function(S, $, console){
+  'use strict';
+
+  S.ReviewsSummaryView = Backbone.View.extend({
+    events: {
+      // 'submit .user-review': 'onFormSubmit'
+    },
+
+    // Initializer
+    // ===========
+
+    initialize: function() {
+      this.bindToPlaceCollection(this.collection);
+    },
+
+    // Event binding & unbinding methods
+    // =================================
+
+    bindToPlaceCollection: function(collection) {
+      collection.on('reset', this.handleResetPlaces, this);
+      collection.on('add', this.handleAddPlace, this);
+      collection.on('remove', this.handleRemovePlace, this);
+    },
+
+    bindToPlaceReviews: function(model) {
+      var collection = model.submissionSets['reviews'];
+
+      if (!collection) {
+        collection = models.submissionSets['reviews'] = S.SubmissionCollection(models, {
+          submissionType: 'reviews',
+          placeModel: model
+        });
+      }
+
+      collection.on('reset', this.handleChangeReviews, this);
+      collection.on('add', this.handleChangeReviews, this);
+      collection.on('remove', this.handleChangeReviews, this);
+      collection.on('change', this.handleChangeReviews, this);
+//      collection.forEach(this.bindToReviewChanges, this);
+    },
+
+    unbindFromPlaceReviews: function(model) {
+      var collection = model.submissionSets['reviews'];
+
+      if (!collection) return;
+
+      collection.off('reset', this.handleChangeReviews, this);
+      collection.off('add', this.handleChangeReviews, this);
+      collection.off('remove', this.handleChangeReviews, this);
+      collection.off('change', this.handleChangeReviews, this);
+      // collection.forEach(this.unbindFromReviewChanges, this);
+    },
+
+    bindToReviewChanges: function(model) {
+      model.on('change', this.handleChangeReviews, this);
+    },
+
+    unbindFromReviewChanges: function(model) {
+      model.off('change', this.handleChangeReviews, this);
+    },
+
+    // Event handlers
+    // ==============
+
+    handleAddPlace: function(model) {
+      this.bindToPlaceReviews(model);
+      this.updateReviewsDisplays();
+    },
+
+    handleRemovePlace: function(model) {
+      this.unbindFromPlaceReviews(model);
+      this.updateReviewsDisplays();
+    },
+
+    handleResetPlaces: function(collection) {
+      collection.forEach(this.bindToPlaceReviews, this);
+      this.updateReviewsDisplays();
+    },
+
+    handleChangeReviews: function(model) {
+      this.updateReviewsDisplays();
+    },
+
+    // Utility functions
+    // =================
+
+    getReviewsCounts: function(reviewOptions, categoryOptions) {
+      var total = 0;
+      var byReview = {};
+      var byReviewCategory = {};
+
+      // Initialize the object for reviews by category.
+      reviewOptions.forEach(function(rev) {
+        byReview[rev] = 0;
+        byReviewCategory[rev] = {};
+        categoryOptions.forEach(function(cat) {
+          byReviewCategory[rev][cat] = 0;
+        });
+      });
+
+      // Calculate the total reviews submitted, as well as how
+      // many reviews fall into each category (location_type).
+      this.collection.forEach(function(place) {
+        if (!place.submissionSets['reviews']) return;
+
+        var review = place.submissionSets['reviews'].first();
+        var rev, cat;
+
+        if (review) {
+          cat = place.get('location_type');
+          rev = review.get('review');
+          if (!rev) return;
+
+          ++total;
+          byReview[rev] += 1;
+          byReviewCategory[rev][cat] += 1;
+        }
+      });
+
+      return {total: total, byReview: byReview, byReviewCategory: byReviewCategory};
+    },
+
+    getStaffGroupCount: function(categoryOptions) {
+      // NOTE: Remember, unlike ratings, all ideas belong to the same staff group.
+      var total = 0;
+      var byCategory = {};
+
+      // Initialize the object for places by category.
+      categoryOptions.forEach(function(cat) {
+        byCategory[cat] = 0;
+      });
+
+      this.collection.forEach(function(place) {
+        var cat = place.get('location_type');
+        ++total;
+        byCategory[cat] += 1;
+      });
+      return {total: total, byCategory: byCategory};
+    },
+
+    // UI initial render and update functions
+    // ======================================
+
+    render: function() {
+      var self = this;
+      var reviewLabels = ['Yes', 'Maybe', 'No']
+      var reviewOptions = ['yes', 'maybe', 'no'];
+      var reviewsConfig = this.options.reviews = _.map(
+        _.zip(reviewLabels, reviewOptions),
+        function(l_o) { return {label: l_o[0], option: l_o[1]}; }
+      );
+      var categoriesConfig = this.options.categories;
+
+      // I don't understand why we need to redelegate the event here, but they
+      // are definitely unbound after the first render.
+      this.delegateEvents();
+      this.$el.html(Handlebars.templates['place-list-reviews-summary']());
+
+      // Set up the initial values and templates
+      this.initReviewsChart(reviewsConfig, categoriesConfig);
+
+      // Count reviews and projects in each category
+      this.updateReviewsDisplays();
+
+      return this;
+    },
+
+    updateReviewsDisplays: _.throttle(function() {
+      var reviewOptions = _.pluck(this.options.reviews, 'option');
+      var categoryOptions = _.pluck(this.options.categories, 'option');
+      var reviewsCounts = this.getReviewsCounts(reviewOptions, categoryOptions);
+      var staffGroupCount = this.getStaffGroupCount(reviewOptions);
+
+      this.updateReviewsCount(reviewsCounts.total, staffGroupCount.total);
+      this.updateReviewsChart(reviewsCounts.byReviewCategory, reviewsCounts.byReview, reviewsCounts.total);
+    }, 100),
+
+    // Update the displayed count of completed reviews
+    updateReviewsCount: function(countRated, totalToRate) {
+      var countTemplate = Handlebars.templates['place-list-reviews-count'];
+      this.$el.find('.reviews-count-wrapper').html(countTemplate({
+        count: countRated,
+        total: totalToRate
+      }));
+    },
+
+    // Initialize the structure for the reviews stacked bar chart
+    initReviewsChart: function(reviewsConfig, categoryConfig) {
+      var chartTemplate = Handlebars.templates['place-list-reviews-chart'];
+      this.$el.find('.reviews-chart-wrapper').html(chartTemplate({
+        reviews: reviewsConfig,
+        categories: categoryConfig
+      }));
+    },
+
+    // Update the reviews counts in the chart; avoid re-rendering the markup so
+    // that values can animate.
+    //
+    // * countByReviewCategory
+    //   A 2-dimensional mapping from review-category pair to the number of
+    //   ideas that the current user has reviewed in that category with that
+    //   review.
+    //
+    // * countByReview
+    //   A mapping from review value to the total number of ideas that the user
+    //   has given that review.
+    //
+    // * countTotal
+    //   The total number of ideas that the current user has reviewed.
+    //
+    updateReviewsChart: function(countByReviewCategory, countByReview, countTotal) {
+      var countMax = _.max(_.values(countByReview));
+
+      // Scale the chart appropriately, but in increments so as to not be too
+      // jarring
+      var scaleFactor = 1.0;
+      var tickStep = 3;
+           if (countMax > 300) { scaleFactor = 0.05;   tickStep = 60; }
+      else if (countMax > 200) { scaleFactor = 0.0667; tickStep = 50; }
+      else if (countMax > 80)  { scaleFactor = 0.1;    tickStep = 30; }
+      else if (countMax > 60)  { scaleFactor = 0.25;   tickStep = 12; }
+      else if (countMax > 40)  { scaleFactor = 0.333;  tickStep = 10; }
+      else if (countMax > 30)  { scaleFactor = 0.5;    tickStep = 6; }
+      else if (countMax > 20)  { scaleFactor = 0.667;  tickStep = 5; }
+
+      // First resize all of the chart bar segments
+      this.$el.find('.reviews-chart-bar').each(function(barIndex, bar) {
+        var rat = $(bar).attr('data-review');
+        var ratTotal = countByReview[rat];
+        var ratShare = 1.0 * ratTotal / countMax;
+        $(bar).find('.reviews-chart-segment').each(function(segIndex, segment) {
+          var cat = $(segment).attr('data-category');
+          var count = countByReviewCategory[rat][cat] || 0;
+          $(segment)
+            .css('width', (count * scaleFactor) + 'em')
+            .attr('title', count + ' ' + $(segment).attr('data-category-label') + ' idea(s) with review of ' + rat)
+        });
+      });
+
+      // Update the review totals
+      this.$el.find('.reviews-chart-review-total').each(function(index, label) {
+        var rat = $(label).attr('data-review');
+        var ratTotal = countByReview[rat];
+        $(label)
+          .html(ratTotal ? ratTotal : '');
+      });
+
+      // Set the initial margin before the tick labels
+      this.$el.find('.reviews-chart-tick-labels-wrapper')
+        .css('margin-left', ((tickStep * scaleFactor) / 2.0 + 4.7) + 'em');
+
+      // Set the spacing between tick labels
+      this.$el.find('.reviews-chart-tick-label').each(function(labelIndex, label) {
+        $(label)
+          .html(tickStep * (labelIndex + 1))
+          .css('width', (tickStep * scaleFactor * 1.5) + 'em');
+      });
+
+      // Set the spacing between ticks
+      this.$el.find('.reviews-chart-tick').each(function(tickIndex, tick) {
+        $(tick)
+          .css('width', (tickStep * scaleFactor) + 'em');
+      });
+    }
+  });
+
+})(Shareabouts, jQuery, Shareabouts.Util.console);
